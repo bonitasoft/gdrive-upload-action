f2d7278e509134f3cfc67e3097e0ec8bdf1b1933
Adrien Kantcheff <adrien.kantcheff@bonitasoft.com>
2024-01-16 15:25:14 +0100

    wip(gh-action): gdrive upload action

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as google from '@googleapis/drive'
+import * as fs from 'fs'
+
+// Google Authorization scopes
+const SCOPES = ['https://www.googleapis.com/auth/drive.file']
+
+// Init Google Drive API instance
+const drive = initDriveAPI()
+
+function initDriveAPI(): google.drive_v3.Drive {
+  const credentials = core.getInput('credentials', { required: true })
+  const credentialsJSON = JSON.parse(
+    Buffer.from(credentials, 'base64').toString()
+  )
+  const auth = new google.auth.GoogleAuth({
+    credentials: credentialsJSON,
+    scopes: SCOPES
+  })
+  return google.drive({ version: 'v3', auth })
+}
 
 /**
  * The main function for the action.
@@ -7,20 +26,87 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    // Get inputs
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // Set outputs
+    core.setOutput('TODO', 'TODO')
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else {
+      core.setFailed(JSON.stringify(error))
+    }
   }
+}
+
+async function upload(): Promise<void> {
+  // TODO
+}
+
+async function getFileId(
+  parentId: string,
+  fileName: string
+): Promise<string | null> {
+  core.debug(`Getting file with name '${fileName}' under folder '${parentId}'`)
+  const requestParams: google.drive_v3.Params$Resource$Files$List = {
+    q: `name='${fileName}' and '${parentId}' in parents and trashed=false`,
+    fields: 'files(id)',
+    includeItemsFromAllDrives: true,
+    supportsAllDrives: true
+  }
+  const response = await drive.files.list(requestParams)
+
+  const files: google.drive_v3.Schema$File[] | undefined = response.data.files
+  if (files === undefined || files.length === 0) {
+    core.debug(`No entry match the file name '${fileName}'`)
+    return null
+  }
+  if (files.length > 1) {
+    throw new Error(`More than one entry match the file name '${fileName}'`)
+  }
+  return files[0].id || null
+}
+
+async function createFolder(
+  parentId: string,
+  folderName: string
+): Promise<string | null> {
+  core.debug(`Creating folder '${folderName}' under folder '${parentId}'`)
+  const requestParams: google.drive_v3.Params$Resource$Files$Create = {
+    requestBody: {
+      parents: [parentId],
+      name: folderName,
+      mimeType: 'application/vnd.google-apps.folder'
+    },
+    fields: 'id',
+    supportsAllDrives: true
+  }
+  const response = await drive.files.create(requestParams)
+  const folder: google.drive_v3.Schema$File = response.data
+  core.debug(`Folder id: ${folder.id}`)
+  return folder.id || null
+}
+
+async function createFile(
+  parentId: string,
+  fileName: string | null,
+  filePath: string
+): Promise<string | null> {
+  core.debug(`Creating file '${fileName}' under folder '${parentId}'`)
+  const requestParams: google.drive_v3.Params$Resource$Files$Create = {
+    requestBody: {
+      parents: [parentId],
+      name: fileName
+    },
+    media: {
+      body: fs.createReadStream(filePath)
+    },
+    fields: 'id',
+    supportsAllDrives: true
+  }
+  const response = await drive.files.create(requestParams)
+  const file: google.drive_v3.Schema$File = response.data
+  core.debug(`File id: ${file.id}`)
+  return file.id || null
 }
